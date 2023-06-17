fc01fb4cb860d57bf1c0a8b02acfb6ec4447cb46
exequiel <conan34681335@gmail.com>
2023-06-17 20:37:14 -0300

    prueba renderizado

--- src/main/java/com/ProtFolio/APi/controller/Controller.java
+++ src/main/java/com/ProtFolio/APi/controller/Controller.java
@@ -34,12 +34,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class Controller {
 
-    @GetMapping("/health")
-public ResponseEntity<?> checkHealth() {
-  return ResponseEntity.ok().build();
-}
+ 
    @Autowired
    private IUsuarioService userService;
+   @Autowired
+    private IEducacionService eduService;
+   @Autowired
+    public IExperienciaService expService;
+   @Autowired
+    public IEmpleoService empService;
+   @Autowired
+    public IProyectoService proService;
+   
    
    @PostMapping("/nueva/descripcion")
    @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -54,28 +60,8 @@ public ResponseEntity<?> actualizarDescripcion(@RequestBody Usuario usuario) {
   }
 }
 
-/*
-@PostMapping("/perfil/foto")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
-public ResponseEntity<?> actualizarFotoPerfil(@RequestParam("id") Long id, @RequestParam("foto") MultipartFile foto) {
-  try {
-    Usuario usuarioExistente = userService.encontrarPorId(id);
-    // Guardar la nueva foto de perfil en el almacenamiento (por ejemplo, en el sistema de archivos o en Amazon S3)
-    String nuevaRutaFoto = almacenarFotoPerfil(foto);
-    // Actualizar la ruta de la foto de perfil en la base de datos
-    usuarioExistente.setRutaFotoPerfil(nuevaRutaFoto);
-    userService.guardar(usuarioExistente);
-    return ResponseEntity.ok().build();
-  } catch (Exception e) {
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-  }
-}
-*/
-
-
-  
 @PostMapping("/login")
-@CrossOrigin(origins = "https://portfolio-backend-exequiel.web.app")
+@CrossOrigin(origins = "https://portfolio-backend-exequiel.web.app/")
 public ResponseEntity<?> login(@RequestBody Map<String, String> credentials) {
     String email = credentials.get("email");
     String password = credentials.get("password");
@@ -86,117 +72,96 @@ public ResponseEntity<?> login(@RequestBody Map<String, String> credentials) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
-  
-   
-   
-    
+
    @PostMapping("/nuevo/usuario")
    public void agregarUsuario(@RequestBody Usuario user){
        userService.agregarUsuario(user);
-
-
-
    }
+   
   @GetMapping("/ver/usuario")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
-@ResponseBody
-public List<Usuario> verUsuarios(){
-    return userService.verUsuarios();
+    @CrossOrigin(origins = "https://portfolio-backend-exequiel.web.app/", allowedHeaders = "*")
+    @ResponseBody
+    public List<Usuario> verUsuarios(){
+        return userService.verUsuarios();
 }
-   
-   
-
-
-   
+    
     @DeleteMapping("/borrarUsuario/{id}")
     public void borrarUsuario(@PathVariable Long id){
         userService.borrarUsuario(id);
     }
 
-
-    @Autowired
-    private IEducacionService eduService;
-    
     @PostMapping("/nuevo/educacion")
-    
     public void agregarEducacion(@RequestBody Educacion educ){
         eduService.agregarEducacion(educ);
     }
+    
     @GetMapping("/ver/educacion")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @ResponseBody
-    
     public List<Educacion> verEducacion(){
         return eduService.verEducacion();
     }
+    
     @DeleteMapping("/borrarEducacion/{id}")
     public void borrarEducacion(Long id){
         eduService.borrarEducacion(id);
     }
     
-    
-    
-    @Autowired
-    public IExperienciaService expService;
-    
     @PostMapping("/nuevo/experiencia")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public void agregarExperiencia(@RequestBody Experiencia exp){
         expService.agregarExperiencia(exp);
     }
+    
     @GetMapping("/ver/experiencia")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @ResponseBody
-    
     public List<Experiencia> verExperiencia(){
         return expService.verExperiencia();
     }
+    
     @DeleteMapping("/borrarExperiencia/{id}")
     public void borrarExperiencia(Long id){
         expService.borrarExperiencia(id);
     }
     
-@Autowired
-public IEmpleoService empService;
-
-@PostMapping("/nuevo/empleo")
-public void agregarEmpleo (@RequestBody Empleo emp){
-    empService.agregarEmpleo(emp);
+    @PostMapping("/nuevo/empleo")
+    public void agregarEmpleo (@RequestBody Empleo emp){
+        empService.agregarEmpleo(emp);
 }
-@GetMapping("/ver/empleo")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
-@ResponseBody
-
-public List<Empleo> verEmpleo(){
+    
+    @GetMapping("/ver/empleo")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @ResponseBody
+    public List<Empleo> verEmpleo(){
     return empService.verEmpleo();
 }
-@DeleteMapping("/borrarEmpleo/{id}")
+    
+    @DeleteMapping("/borrarEmpleo/{id}")
     public void borarEmpleo(@PathVariable Long id){
         empService.borrarEmpleo(id);
     }
 
-    @Autowired
-    public IProyectoService proService;
-    
     @PostMapping("/nuevo/proyecto")
     public void agregarProyecto(@RequestBody Proyecto pro){
         proService.agregarProyecto(pro);
     }
+    
     @GetMapping("/ver/proyecto")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @ResponseBody
     public List<Proyecto> verProyecto(){
         return proService.verProyecto();
     }
+    
     @DeleteMapping("/borrarProyecto/{id}")
     public void borarProyecto(@PathVariable Long id){
         proService.borrarProyecto(id);
     }
+    
     @GetMapping("/buscarProyecto/{id} ")
     @ResponseBody
     public Proyecto buscarProyecto(@PathVariable Long id){
         return proService.buscarProyecto(id);
-    }
-    
-   
-          
+    }      
 }
