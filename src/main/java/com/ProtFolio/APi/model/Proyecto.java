1761a53e3ac5032da9f176ece8fab6c13f827ff3
exequiel <conan34681335@gmail.com>
2023-06-23 19:49:30 -0300

    lazy loading

--- src/main/java/com/ProtFolio/APi/model/Proyecto.java
+++ src/main/java/com/ProtFolio/APi/model/Proyecto.java
@@ -2,9 +2,12 @@
 package com.ProtFolio.APi.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,6 +21,10 @@ public class Proyecto {
     private String nombre;
     private String descripcion;
     private String url_img;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
     
     public Proyecto(){}
     
